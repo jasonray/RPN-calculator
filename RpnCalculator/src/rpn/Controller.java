d9bf47d6a097ce0e8faebf45645d1279cd15dc70
Jason Ray <jasonray@jayray.net>
2011-05-21 08:30:38 -0400

    moved registry out of controller

--- RpnCalculator/src/rpn/Controller.java
+++ RpnCalculator/src/rpn/Controller.java
@@ -1,28 +1,18 @@
 package rpn;
 
-import java.util.ArrayList;
+import java.util.List;
 
-import rpn.operator.AddOperator;
-import rpn.operator.FactorialOperator;
-import rpn.operator.MultiplyOperator;
 import rpn.operator.Operator;
-import rpn.operator.SubtractionOperator;
 
 public class Controller {
 	private RpnStack numbers = new RpnStack();
+	private List<Operator> operatorRegistry = OperationRegistry.getRegistry();
 
 	public void enter(Integer operand) {
 		numbers.push(operand);
 	}
 
 	public int perform(String operatorCharacter) {
-
-		ArrayList<Operator> operatorRegistry = new ArrayList<Operator>();
-		operatorRegistry.add(new AddOperator());
-		operatorRegistry.add(new SubtractionOperator());
-		operatorRegistry.add(new MultiplyOperator());
-		operatorRegistry.add(new FactorialOperator());
-
 		int result = 0;
 		for (Operator operator : operatorRegistry) {
 			if (operator.handlesOperatorCharacter(operatorCharacter)) {
