b954dbcb6701e29dafd4e135df1f7ebb8d285cff
Jason Ray <jasonray@jayray.net>
2011-05-22 07:36:53 -0400

    added additional average test

--- RpnCalculator/test/rpn/AverageTest.java
+++ RpnCalculator/test/rpn/AverageTest.java
@@ -40,4 +40,13 @@ public class AverageTest {
 		int result = controller.perform("AVERAGE");
 		assertEquals(3, result);
 	}
+
+	@Test
+	public void useAve() {
+		Controller controller = new Controller();
+		controller.enter(1);
+		controller.enter(3);
+		int result = controller.perform("AVE");
+		assertEquals(2, result);
+	}
 }
