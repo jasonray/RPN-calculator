47591de7e649407e98ee76b2214c1553e60d87e7
Jason Ray <jasonray@jayray.net>
2011-05-22 07:33:01 -0400

    added an average operator

---
+++ RpnCalculator/src/rpn/operator/AverageOperator.java
@@ -0,0 +1,30 @@
+package rpn.operator;
+
+import rpn.RpnStack;
+
+public class AverageOperator implements Operator {
+
+	@Override
+	public int doOperation(RpnStack numbers) {
+		int runningTotal = 0;
+		int runningCount = 0;
+		while (!numbers.isEmpty()) {
+			runningCount++;
+			runningTotal += numbers.pop();
+		}
+		int ave = 0;
+		if (runningCount > 0) {
+			ave = runningTotal / runningCount;
+		}
+		numbers.push(ave);
+		return ave;
+
+	}
+
+	@Override
+	public boolean handlesOperand(String operand) {
+		return "AVERAGE".contentEquals(operand.toUpperCase()) || "AVE".contentEquals(operand.toUpperCase());
+
+	}
+
+}
---
+++ RpnCalculator/test/rpn/AverageTest.java
@@ -0,0 +1,43 @@
+package rpn;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AverageTest {
+	@Test
+	public void averageNoNumbers() {
+		Controller controller = new Controller();
+		int result = controller.perform("AVERAGE");
+		assertEquals(0, result);
+	}
+
+	@Test
+	public void averageOneNumber() {
+		Controller controller = new Controller();
+		controller.enter(1);
+		int result = controller.perform("AVERAGE");
+		assertEquals(1, result);
+	}
+
+	@Test
+	public void averageTwoNumber() {
+		Controller controller = new Controller();
+		controller.enter(1);
+		controller.enter(3);
+		int result = controller.perform("AVERAGE");
+		assertEquals(2, result);
+	}
+
+	@Test
+	public void averageFiveNumber() {
+		Controller controller = new Controller();
+		controller.enter(1);
+		controller.enter(2);
+		controller.enter(3);
+		controller.enter(4);
+		controller.enter(5);
+		int result = controller.perform("AVERAGE");
+		assertEquals(3, result);
+	}
+}
