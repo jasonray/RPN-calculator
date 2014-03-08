3dd73ab208b9a616ce6ba9c8808b12902c137930
Jason Ray <jason.ray@agilex.com>
2014-03-08 16:35:08 -0500

    Restructured project.  Removed Scala version (its now in its own repo).  Added gradle

--- RpnCalculator-Scala/src/rpn/scala/operators/Operator.scala
+++
@@ -1,7 +0,0 @@
-package rpn.scala.operators
-import rpn.scala.RpnStack
-
-trait Operator {
-  def performOperation(numbers: RpnStack): Int
-  def handlesOperatorCharacter(operand: String): Boolean
-}
