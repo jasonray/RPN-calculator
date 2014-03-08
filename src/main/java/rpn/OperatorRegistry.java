0f0ade1d97c1922c300b1ce317616b9884e6f6b9
Jason Ray <jason.ray@agilex.com>
2014-03-08 17:04:57 -0500

    added fib operator

--- src/main/java/rpn/OperatorRegistry.java
+++ src/main/java/rpn/OperatorRegistry.java
@@ -10,6 +10,7 @@ import rpn.operator.ClearOperator;
 import rpn.operator.DivisionOperator;
 import rpn.operator.ExponentiationOperator;
 import rpn.operator.FactorialOperator;
+import rpn.operator.FibonacciOperator;
 import rpn.operator.MultiplicationOperator;
 import rpn.operator.Operator;
 import rpn.operator.ReverseOperator;
@@ -17,34 +18,35 @@ import rpn.operator.SubtractionOperator;
 
 public class OperatorRegistry {
 
-	private List<Operator> registry = getRegistry();
+    private List<Operator> registry = getRegistry();
 
-	private List<Operator> getRegistry() {
-		List<Operator> operatorRegistry;
-		operatorRegistry = new ArrayList<Operator>();
-		operatorRegistry.add(new AdditionOperator());
-		operatorRegistry.add(new SubtractionOperator());
-		operatorRegistry.add(new MultiplicationOperator());
-		operatorRegistry.add(new DivisionOperator());
-		operatorRegistry.add(new FactorialOperator());
-		operatorRegistry.add(new ReverseOperator());
-		operatorRegistry.add(new AverageOperator());
-		operatorRegistry.add(new ClearOperator());
-		operatorRegistry.add(new ExponentiationOperator());
-		operatorRegistry.add(new AbsoluteOperator());
-		return operatorRegistry;
-	}
+    private List<Operator> getRegistry() {
+        List<Operator> operatorRegistry;
+        operatorRegistry = new ArrayList<Operator>();
+        operatorRegistry.add(new AdditionOperator());
+        operatorRegistry.add(new SubtractionOperator());
+        operatorRegistry.add(new MultiplicationOperator());
+        operatorRegistry.add(new DivisionOperator());
+        operatorRegistry.add(new FactorialOperator());
+        operatorRegistry.add(new ReverseOperator());
+        operatorRegistry.add(new AverageOperator());
+        operatorRegistry.add(new ClearOperator());
+        operatorRegistry.add(new ExponentiationOperator());
+        operatorRegistry.add(new AbsoluteOperator());
+        operatorRegistry.add(new FibonacciOperator());
+        return operatorRegistry;
+    }
 
-	public Operator getOperator(String operatorCharacter) {
-		Operator relevantOperator = null;
-		if (operatorCharacter != null && !operatorCharacter.contentEquals("")) {
-			for (Operator operator : registry) {
-				if (operator.handlesOperatorCharacter(operatorCharacter)) {
-					relevantOperator = operator;
-					break;
-				}
-			}
-		}
-		return relevantOperator;
-	}
+    public Operator getOperator(String operatorCharacter) {
+        Operator relevantOperator = null;
+        if (operatorCharacter != null && !operatorCharacter.contentEquals("")) {
+            for (Operator operator : registry) {
+                if (operator.handlesOperatorCharacter(operatorCharacter)) {
+                    relevantOperator = operator;
+                    break;
+                }
+            }
+        }
+        return relevantOperator;
+    }
 }
