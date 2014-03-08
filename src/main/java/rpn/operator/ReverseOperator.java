90caa50d115fcbaeaa5f3139f346b2e598d77064
Jason Ray <jason.ray@agilex.com>
2014-03-08 16:57:58 -0500

    formatting

--- src/main/java/rpn/operator/ReverseOperator.java
+++ src/main/java/rpn/operator/ReverseOperator.java
@@ -4,24 +4,23 @@ import rpn.RpnStack;
 
 public class ReverseOperator implements Operator {
 
-	@Override
-	public int doOperation(RpnStack numbers) {
-		RpnStack tempStackA = new RpnStack();
-		RpnStack tempStackB = new RpnStack();
-		reverseCopyStack(numbers, tempStackA);
-		reverseCopyStack(tempStackA, tempStackB);
-		reverseCopyStack(tempStackB, numbers);
-		return numbers.peek();
-	}
+    @Override
+    public int doOperation(RpnStack numbers) {
+        RpnStack tempStackA = new RpnStack();
+        RpnStack tempStackB = new RpnStack();
+        reverseCopyStack(numbers, tempStackA);
+        reverseCopyStack(tempStackA, tempStackB);
+        reverseCopyStack(tempStackB, numbers);
+        return numbers.peek();
+    }
 
-	private void reverseCopyStack(RpnStack from, RpnStack to) {
-		while (!from.isEmpty())
-			to.push(from.pop());
-	}
+    private void reverseCopyStack(RpnStack from, RpnStack to) {
+        while (!from.isEmpty())
+            to.push(from.pop());
+    }
 
-	@Override
-	public boolean handlesOperatorCharacter(String operand) {
-		return "REVERSE".contentEquals(operand.toUpperCase()) || "REV".contentEquals(operand.toUpperCase());
-	}
-
+    @Override
+    public boolean handlesOperatorCharacter(String operand) {
+        return "REVERSE".contentEquals(operand.toUpperCase()) || "REV".contentEquals(operand.toUpperCase());
+    }
 }
