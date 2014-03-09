6697a562a1fd75a0c4aa67061052ae9d2fef6dbf
Jason Ray <jason.ray@agilex.com>
2014-03-09 12:44:18 -0400

    formatting

--- src/test/java/rpn/SubtractTest.java
+++ src/test/java/rpn/SubtractTest.java
@@ -1,52 +1,52 @@
 package rpn;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
 public class SubtractTest {
-	@Test
-	public void subtractNoNumbers() {
-		RpnCalculator calc = new RpnCalculator();
-		int result = calc.perform("-");
-		assertEquals(0, result);
-	}
+    @Test
+    public void subtractNoNumbers() {
+        RpnCalculator calc = new RpnCalculator();
+        int result = calc.perform("-");
+        assertEquals(0, result);
+    }
 
-	@Test
-	public void subtractOneNumbers() {
-		RpnCalculator calc = new RpnCalculator();
-		calc.enter(1);
-		int result = calc.perform("-");
-		assertEquals(-1, result);
-	}
+    @Test
+    public void subtractOneNumbers() {
+        RpnCalculator calc = new RpnCalculator();
+        calc.enter(1);
+        int result = calc.perform("-");
+        assertEquals(-1, result);
+    }
 
-	@Test
-	public void subtractOneNumberTwice() {
-		RpnCalculator calc = new RpnCalculator();
-		calc.enter(1);
-		int result = calc.perform("-");
-		result = calc.perform("-");
-		assertEquals(1, result);
-	}
+    @Test
+    public void subtractOneNumberTwice() {
+        RpnCalculator calc = new RpnCalculator();
+        calc.enter(1);
+        int result = calc.perform("-");
+        result = calc.perform("-");
+        assertEquals(1, result);
+    }
 
-	@Test
-	public void subtractTwoNumbers() {
-		RpnCalculator calc = new RpnCalculator();
-		calc.enter(20);
-		calc.enter(10);
-		int result = calc.perform("-");
-		assertEquals(10, result);
-	}
+    @Test
+    public void subtractTwoNumbers() {
+        RpnCalculator calc = new RpnCalculator();
+        calc.enter(20);
+        calc.enter(10);
+        int result = calc.perform("-");
+        assertEquals(10, result);
+    }
 
-	@Test
-	public void subtractThreeNumbers() {
-		RpnCalculator calc = new RpnCalculator();
-		calc.enter(21);
-		calc.enter(13);
-		calc.perform("-");
-		calc.enter(5);
+    @Test
+    public void subtractThreeNumbers() {
+        RpnCalculator calc = new RpnCalculator();
+        calc.enter(21);
+        calc.enter(13);
+        calc.perform("-");
+        calc.enter(5);
 
-		int result = calc.perform("-");
-		assertEquals(3, result);
-	}
+        int result = calc.perform("-");
+        assertEquals(3, result);
+    }
 }
