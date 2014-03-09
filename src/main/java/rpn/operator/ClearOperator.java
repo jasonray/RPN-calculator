07d4797530d7dd5c4ceff88a6c49bf6854bb5ab5
Jason Ray <jason.ray@agilex.com>
2014-03-09 12:40:31 -0400

    formatting

--- src/main/java/rpn/operator/ClearOperator.java
+++ src/main/java/rpn/operator/ClearOperator.java
@@ -4,20 +4,20 @@ import rpn.RpnStack;
 
 public class ClearOperator implements Operator {
 
-	private static final String C = "C";
-	private static final String CLEAR = "CLEAR";
+    private static final String C = "C";
+    private static final String CLEAR = "CLEAR";
 
-	@Override
-	public int doOperation(RpnStack numbers) {
-		while (!numbers.isEmpty())
-			numbers.pop();
-		return 0;
-	}
+    @Override
+    public int doOperation(RpnStack numbers) {
+        while (!numbers.isEmpty())
+            numbers.pop();
+        return 0;
+    }
 
-	@Override
-	public boolean handlesOperatorCharacter(String operand) {
-		return C.contentEquals(operand.toUpperCase()) || CLEAR.contentEquals(operand.toUpperCase());
+    @Override
+    public boolean handlesOperatorCharacter(String operand) {
+        return C.contentEquals(operand.toUpperCase()) || CLEAR.contentEquals(operand.toUpperCase());
 
-	}
+    }
 
 }
