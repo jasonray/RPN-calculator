b2006b05282f73fcf274e80f8da0392c73b1a10b
Jason Ray <jason.ray@agilex.com>
2014-03-09 12:42:25 -0400

    formatting

--- src/test/java/rpn/DivisionTest.java
+++ src/test/java/rpn/DivisionTest.java
@@ -5,53 +5,53 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class DivisionTest {
-	@Test
-	public void divideTwoNumbers() {
-		RpnCalculator calc = new RpnCalculator();
-		calc.enter(8);
-		calc.enter(2);
-		int result = calc.perform("/");
-		assertEquals(4, result);
-	}
+    @Test
+    public void divideTwoNumbers() {
+        RpnCalculator calc = new RpnCalculator();
+        calc.enter(8);
+        calc.enter(2);
+        int result = calc.perform("/");
+        assertEquals(4, result);
+    }
 
-	@Test
-	public void divideOneNumber() {
-		RpnCalculator calc = new RpnCalculator();
-		calc.enter(2);
-		int result = calc.perform("/");
-		assertEquals(0, result);
-	}
+    @Test
+    public void divideOneNumber() {
+        RpnCalculator calc = new RpnCalculator();
+        calc.enter(2);
+        int result = calc.perform("/");
+        assertEquals(0, result);
+    }
 
-	@Test(expected = ArithmeticException.class)
-	public void divideNoNumber() {
-		RpnCalculator calc = new RpnCalculator();
-		calc.perform("/");
-	}
+    @Test(expected = ArithmeticException.class)
+    public void divideNoNumber() {
+        RpnCalculator calc = new RpnCalculator();
+        calc.perform("/");
+    }
 
-	@Test
-	public void divideThreeNumber() {
-		RpnCalculator calc = new RpnCalculator();
-		calc.enter(16);
-		calc.enter(8);
-		calc.enter(2);
-		int result = calc.perform("/");
-		assertEquals(4, result);
-	}
+    @Test
+    public void divideThreeNumber() {
+        RpnCalculator calc = new RpnCalculator();
+        calc.enter(16);
+        calc.enter(8);
+        calc.enter(2);
+        int result = calc.perform("/");
+        assertEquals(4, result);
+    }
 
-	@Test(expected = ArithmeticException.class)
-	public void divideByZero() {
-		RpnCalculator calc = new RpnCalculator();
-		calc.enter(6);
-		calc.enter(0);
-		calc.perform("/");
-	}
+    @Test(expected = ArithmeticException.class)
+    public void divideByZero() {
+        RpnCalculator calc = new RpnCalculator();
+        calc.enter(6);
+        calc.enter(0);
+        calc.perform("/");
+    }
 
-	@Test
-	public void divideByFractionalNumber() {
-		RpnCalculator calc = new RpnCalculator();
-		calc.enter(6);
-		calc.enter(4);
-		int result = calc.perform("/");
-		assertEquals(1, result);
-	}
+    @Test
+    public void divideByFractionalNumber() {
+        RpnCalculator calc = new RpnCalculator();
+        calc.enter(6);
+        calc.enter(4);
+        int result = calc.perform("/");
+        assertEquals(1, result);
+    }
 }
