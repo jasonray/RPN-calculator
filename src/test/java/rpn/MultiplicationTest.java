0179ee30d46f2782dc7f79a3d488630e27c201da
Jason Ray <jason.ray@agilex.com>
2014-03-09 12:43:19 -0400

    formatting

--- src/test/java/rpn/MultiplicationTest.java
+++ src/test/java/rpn/MultiplicationTest.java
@@ -5,47 +5,47 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class MultiplicationTest {
-	@Test
-	public void multipleTwoNumbers() {
-		RpnCalculator calc = new RpnCalculator();
-		calc.enter(2);
-		calc.enter(4);
-		int result = calc.perform("*");
-		assertEquals(8, result);
-	}
+    @Test
+    public void multipleTwoNumbers() {
+        RpnCalculator calc = new RpnCalculator();
+        calc.enter(2);
+        calc.enter(4);
+        int result = calc.perform("*");
+        assertEquals(8, result);
+    }
 
-	@Test
-	public void multipleNegNumbers() {
-		RpnCalculator calc = new RpnCalculator();
-		calc.enter(-2);
-		calc.enter(4);
-		int result = calc.perform("*");
-		assertEquals(-8, result);
-	}
+    @Test
+    public void multipleNegNumbers() {
+        RpnCalculator calc = new RpnCalculator();
+        calc.enter(-2);
+        calc.enter(4);
+        int result = calc.perform("*");
+        assertEquals(-8, result);
+    }
 
-	@Test
-	public void multiplyNoNumbers() {
-		RpnCalculator calc = new RpnCalculator();
-		int result = calc.perform("*");
-		assertEquals(0, result);
-	}
+    @Test
+    public void multiplyNoNumbers() {
+        RpnCalculator calc = new RpnCalculator();
+        int result = calc.perform("*");
+        assertEquals(0, result);
+    }
 
-	@Test
-	public void multiplyOneNumber() {
-		RpnCalculator calc = new RpnCalculator();
-		calc.enter(1);
-		int result = calc.perform("*");
-		assertEquals(0, result);
-	}
+    @Test
+    public void multiplyOneNumber() {
+        RpnCalculator calc = new RpnCalculator();
+        calc.enter(1);
+        int result = calc.perform("*");
+        assertEquals(0, result);
+    }
 
-	@Test
-	public void multipleThreeNumbers() {
-		RpnCalculator calc = new RpnCalculator();
-		calc.enter(2);
-		calc.enter(3);
-		calc.perform("*");
-		calc.enter(3);
-		int result = calc.perform("*");
-		assertEquals(18, result);
-	}
+    @Test
+    public void multipleThreeNumbers() {
+        RpnCalculator calc = new RpnCalculator();
+        calc.enter(2);
+        calc.enter(3);
+        calc.perform("*");
+        calc.enter(3);
+        int result = calc.perform("*");
+        assertEquals(18, result);
+    }
 }
