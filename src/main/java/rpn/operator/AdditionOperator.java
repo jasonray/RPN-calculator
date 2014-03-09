cdf4058c83e0247a464b84475a05d91ce0019467
Jason Ray <jason.ray@agilex.com>
2014-03-09 12:40:11 -0400

    formatting

--- src/main/java/rpn/operator/AdditionOperator.java
+++ src/main/java/rpn/operator/AdditionOperator.java
@@ -2,16 +2,16 @@ package rpn.operator;
 
 public class AdditionOperator extends BinaryOperator {
 
-	private static final String PLUS = "+";
+    private static final String PLUS = "+";
 
-	@Override
-	protected int doOperation(int lhs, int rhs) {
-		return lhs + rhs;
-	}
+    @Override
+    protected int doOperation(int lhs, int rhs) {
+        return lhs + rhs;
+    }
 
-	@Override
-	public boolean handlesOperatorCharacter(String operand) {
-		return PLUS.contentEquals(operand);
-	}
+    @Override
+    public boolean handlesOperatorCharacter(String operand) {
+        return PLUS.contentEquals(operand);
+    }
 
 }
