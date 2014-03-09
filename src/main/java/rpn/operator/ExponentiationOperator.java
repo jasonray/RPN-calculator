62b8f8d3dfb0e13b3eb92b0d9d47b15f47bcce51
Jason Ray <jason.ray@agilex.com>
2014-03-09 12:40:51 -0400

    formatting

--- src/main/java/rpn/operator/ExponentiationOperator.java
+++ src/main/java/rpn/operator/ExponentiationOperator.java
@@ -2,22 +2,22 @@ package rpn.operator;
 
 public class ExponentiationOperator extends BinaryOperator {
 
-	private static final String POWER = "^";
+    private static final String POWER = "^";
 
-	@Override
-	public boolean handlesOperatorCharacter(String operand) {
-		return POWER.contentEquals(operand);
-	}
+    @Override
+    public boolean handlesOperatorCharacter(String operand) {
+        return POWER.contentEquals(operand);
+    }
 
-	@Override
-	protected int doOperation(int lhs, int rhs) {
-		int product = 1;
-		if (rhs > 0)
-			for (int i = 0; i < rhs; i++)
-				product = product * lhs;
-		else if (rhs < 0)
-			product = 1 / doOperation(lhs, -1 * rhs);
-		return product;
-	}
+    @Override
+    protected int doOperation(int lhs, int rhs) {
+        int product = 1;
+        if (rhs > 0)
+            for (int i = 0; i < rhs; i++)
+                product = product * lhs;
+        else if (rhs < 0)
+            product = 1 / doOperation(lhs, -1 * rhs);
+        return product;
+    }
 
 }
