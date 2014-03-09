605dd5c593fa0b3e508fb90d1526e07fedcc6c7f
Jason Ray <jason.ray@agilex.com>
2014-03-09 12:43:05 -0400

    formatting

--- src/test/java/rpn/UnknownOperationTest.java
+++ src/test/java/rpn/UnknownOperationTest.java
@@ -3,17 +3,17 @@ package rpn;
 import org.junit.Test;
 
 public class UnknownOperationTest {
-	@Test(expected = IllegalArgumentException.class)
-	public void unknownOperation() {
-		RpnCalculator controller = new RpnCalculator();
-		controller.perform("?");
-		// expect error
-	}
+    @Test(expected = IllegalArgumentException.class)
+    public void unknownOperation() {
+        RpnCalculator controller = new RpnCalculator();
+        controller.perform("?");
+        // expect error
+    }
 
-	@Test(expected = IllegalArgumentException.class)
-	public void blankOperand() {
-		RpnCalculator controller = new RpnCalculator();
-		controller.perform(null);
-		// expect error
-	}
+    @Test(expected = IllegalArgumentException.class)
+    public void blankOperand() {
+        RpnCalculator controller = new RpnCalculator();
+        controller.perform(null);
+        // expect error
+    }
 }
