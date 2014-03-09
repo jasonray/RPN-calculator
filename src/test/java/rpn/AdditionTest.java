9e0a65be79d9cd9a1d2633f497c3ddb7f6c4c199
Jason Ray <jason.ray@agilex.com>
2014-03-09 12:41:52 -0400

    formatting

--- src/test/java/rpn/AdditionTest.java
+++ src/test/java/rpn/AdditionTest.java
@@ -1,42 +1,42 @@
 package rpn;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
 public class AdditionTest {
-	@Test
-	public void addTwoNumbersReturnsSum() {
-		RpnCalculator calc = new RpnCalculator();
-		calc.enter(30);
-		calc.enter(4);
-		int result = calc.perform("+");
-		assertEquals(34, result);
-	}
+    @Test
+    public void addTwoNumbersReturnsSum() {
+        RpnCalculator calc = new RpnCalculator();
+        calc.enter(30);
+        calc.enter(4);
+        int result = calc.perform("+");
+        assertEquals(34, result);
+    }
 
-	@Test
-	public void addNoNumbers() {
-		RpnCalculator calc = new RpnCalculator();
-		int result = calc.perform("+");
-		assertEquals(0, result);
-	}
+    @Test
+    public void addNoNumbers() {
+        RpnCalculator calc = new RpnCalculator();
+        int result = calc.perform("+");
+        assertEquals(0, result);
+    }
 
-	@Test
-	public void addOneNumber() {
-		RpnCalculator calc = new RpnCalculator();
-		calc.enter(1);
-		int result = calc.perform("+");
-		assertEquals(1, result);
-	}
+    @Test
+    public void addOneNumber() {
+        RpnCalculator calc = new RpnCalculator();
+        calc.enter(1);
+        int result = calc.perform("+");
+        assertEquals(1, result);
+    }
 
-	@Test
-	public void addThreeNumbersReturnsSum() {
-		RpnCalculator calc = new RpnCalculator();
-		calc.enter(1);
-		calc.enter(2);
-		calc.perform("+");
-		calc.enter(3);
-		int result = calc.perform("+");
-		assertEquals(6, result);
-	}
+    @Test
+    public void addThreeNumbersReturnsSum() {
+        RpnCalculator calc = new RpnCalculator();
+        calc.enter(1);
+        calc.enter(2);
+        calc.perform("+");
+        calc.enter(3);
+        int result = calc.perform("+");
+        assertEquals(6, result);
+    }
 }
