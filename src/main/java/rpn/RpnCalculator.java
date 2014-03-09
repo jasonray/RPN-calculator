ce8822c4a4102784d50e88fec18895a3522c5989
Jason Ray <jason.ray@agilex.com>
2014-03-09 12:37:30 -0400

    formatting

--- src/main/java/rpn/RpnCalculator.java
+++ src/main/java/rpn/RpnCalculator.java
@@ -3,20 +3,20 @@ package rpn;
 import rpn.operator.Operator;
 
 public class RpnCalculator {
-	private RpnStack numbers = new RpnStack();
-	private OperatorRegistry operatorRegistry = new OperatorRegistry();
+    private RpnStack numbers = new RpnStack();
+    private OperatorRegistry operatorRegistry = new OperatorRegistry();
 
-	public void enter(Integer operand) {
-		numbers.push(operand);
-	}
+    public void enter(Integer operand) {
+        numbers.push(operand);
+    }
 
-	public int perform(String operatorCharacter) {
-		int result = 0;
-		Operator operatorImplementation = operatorRegistry.getOperator(operatorCharacter);
-		if (operatorImplementation == null)
-			throw new IllegalArgumentException("Unknown Operator " + operatorCharacter);
-		result = operatorImplementation.doOperation(numbers);
-		return result;
-	}
+    public int perform(String operatorCharacter) {
+        int result = 0;
+        Operator operatorImplementation = operatorRegistry.getOperator(operatorCharacter);
+        if (operatorImplementation == null)
+            throw new IllegalArgumentException("Unknown Operator " + operatorCharacter);
+        result = operatorImplementation.doOperation(numbers);
+        return result;
+    }
 
 }
