eb5489fa9838c196bff88aa5a3fd7a324701cf1f
Jason Ray <jason.ray@agilex.com>
2014-03-09 12:40:02 -0400

    formatting

--- src/main/java/rpn/operator/AverageOperator.java
+++ src/main/java/rpn/operator/AverageOperator.java
@@ -4,26 +4,26 @@ import rpn.RpnStack;
 
 public class AverageOperator implements Operator {
 
-	@Override
-	public int doOperation(RpnStack numbers) {
-		int runningTotal = 0;
-		int runningCount = 0;
-		while (!numbers.isEmpty()) {
-			runningCount++;
-			runningTotal += numbers.pop();
-		}
-		int ave = 0;
-		if (runningCount > 0) {
-			ave = runningTotal / runningCount;
-		}
-		numbers.push(ave);
-		return ave;
-	}
+    @Override
+    public int doOperation(RpnStack numbers) {
+        int runningTotal = 0;
+        int runningCount = 0;
+        while (!numbers.isEmpty()) {
+            runningCount++;
+            runningTotal += numbers.pop();
+        }
+        int ave = 0;
+        if (runningCount > 0) {
+            ave = runningTotal / runningCount;
+        }
+        numbers.push(ave);
+        return ave;
+    }
 
-	@Override
-	public boolean handlesOperatorCharacter(String operand) {
-		return "AVERAGE".contentEquals(operand.toUpperCase()) || "AVE".contentEquals(operand.toUpperCase());
+    @Override
+    public boolean handlesOperatorCharacter(String operand) {
+        return "AVERAGE".contentEquals(operand.toUpperCase()) || "AVE".contentEquals(operand.toUpperCase());
 
-	}
+    }
 
 }
