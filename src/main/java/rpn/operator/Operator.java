d4985617b64f40eb175498bf407943e646a4b3f2
Jason Ray <jason.ray@agilex.com>
2014-03-09 12:41:24 -0400

    formatting

--- src/main/java/rpn/operator/Operator.java
+++ src/main/java/rpn/operator/Operator.java
@@ -3,7 +3,7 @@ package rpn.operator;
 import rpn.RpnStack;
 
 public interface Operator {
-	public int doOperation(RpnStack numbers);
+    public int doOperation(RpnStack numbers);
 
-	public boolean handlesOperatorCharacter(String operand);
+    public boolean handlesOperatorCharacter(String operand);
 }
