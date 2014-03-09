303ef272e89608337ecb8e8c5344d569e7a1aa20
Jason Ray <jason.ray@agilex.com>
2014-03-09 12:41:05 -0400

    formatting

--- src/main/java/rpn/operator/FactorialOperator.java
+++ src/main/java/rpn/operator/FactorialOperator.java
@@ -4,23 +4,23 @@ import rpn.RpnStack;
 
 public class FactorialOperator implements Operator {
 
-	private static final String FACTORIAL = "!";
+    private static final String FACTORIAL = "!";
 
-	@Override
-	public int doOperation(RpnStack numbers) {
-		int total = 1;
-		Integer operand = numbers.pop();
-		while (operand > 0) {
-			total = total * operand;
-			operand--;
-		}
-		numbers.push(total);
-		return total;
-	}
+    @Override
+    public int doOperation(RpnStack numbers) {
+        int total = 1;
+        Integer operand = numbers.pop();
+        while (operand > 0) {
+            total = total * operand;
+            operand--;
+        }
+        numbers.push(total);
+        return total;
+    }
 
-	@Override
-	public boolean handlesOperatorCharacter(String operand) {
-		return FACTORIAL.contentEquals(operand);
-	}
+    @Override
+    public boolean handlesOperatorCharacter(String operand) {
+        return FACTORIAL.contentEquals(operand);
+    }
 
 }
