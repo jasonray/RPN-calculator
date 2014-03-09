8a5ea2dd5c96a0934ead8a1ed69cab453031bbfd
Jason Ray <jason.ray@agilex.com>
2014-03-09 12:37:14 -0400

    added fib

---
+++ src/main/java/rpn/operator/FibonacciOperator.java
@@ -0,0 +1,38 @@
+package rpn.operator;
+
+import rpn.RpnStack;
+
+public class FibonacciOperator implements Operator {
+    @Override
+    public int doOperation(RpnStack numbers) {
+        int n = numbers.pop();
+        return fib(n);
+    }
+
+    private int fib(int n) {
+
+        if (n <= 0)
+            return 0;
+        if (n == 1)
+            return 1;
+
+        int prev1 = 1;
+        int prev2 = 0;
+        int result = 0;
+
+        for (int i = 1; i < n; i++) {
+            result = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = result;
+        }
+
+        return result;
+
+    }
+
+    @Override
+    public boolean handlesOperatorCharacter(String operand) {
+        return "FIB".contentEquals(operand.toUpperCase()) || "FIBONACCI".contentEquals(operand.toUpperCase());
+    }
+
+}
---
+++ src/test/java/rpn/FibonacciOperatorTest.java
@@ -0,0 +1,72 @@
+package rpn;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class FibonacciOperatorTest {
+    @Test
+    public void fibEmpty() {
+        RpnCalculator calc = new RpnCalculator();
+        int result = calc.perform("fib");
+        assertEquals(0, result);
+    }
+
+    @Test
+    public void fib0() {
+        runTest(0, 0);
+    }
+
+    @Test
+    public void fib1() {
+        runTest(1, 1);
+    }
+
+    @Test
+    public void fib2() {
+        runTest(2, 1);
+    }
+
+    @Test
+    public void fib3() {
+        runTest(3, 2);
+    }
+
+    @Test
+    public void fib4() {
+        runTest(4, 3);
+    }
+
+    @Test
+    public void fib5() {
+        runTest(5, 5);
+    }
+
+    @Test
+    public void fib6() {
+        runTest(6, 8);
+    }
+
+    @Test
+    public void fib7() {
+        runTest(7, 13);
+    }
+
+    @Test
+    public void fib30() {
+        runTest(30, 832040);
+    }
+
+    @Test
+    public void fib40() {
+        runTest(40, 102334155);
+    }
+
+    private void runTest(int n, int expected) {
+        RpnCalculator calc = new RpnCalculator();
+        calc.enter(n);
+        int result = calc.perform("fib");
+        assertEquals(expected, result);
+    }
+
+}
