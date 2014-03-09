d1bf642ceb4974cbc7cc57d76a0242c3f44ba99c
Jason Ray <jason.ray@agilex.com>
2014-03-09 12:41:15 -0400

    formatting

--- src/main/java/rpn/operator/MultiplicationOperator.java
+++ src/main/java/rpn/operator/MultiplicationOperator.java
@@ -2,16 +2,16 @@ package rpn.operator;
 
 public class MultiplicationOperator extends BinaryOperator {
 
-	private static final String MULTIPLY = "*";
+    private static final String MULTIPLY = "*";
 
-	@Override
-	protected int doOperation(int lhs, int rhs) {
-		return lhs * rhs;
-	}
+    @Override
+    protected int doOperation(int lhs, int rhs) {
+        return lhs * rhs;
+    }
 
-	@Override
-	public boolean handlesOperatorCharacter(String operand) {
-		return MULTIPLY.contentEquals(operand);
-	}
+    @Override
+    public boolean handlesOperatorCharacter(String operand) {
+        return MULTIPLY.contentEquals(operand);
+    }
 
 }
