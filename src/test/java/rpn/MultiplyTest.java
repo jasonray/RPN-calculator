53206f70f62c599cce0fe21182ef5b1da992e069
Jason Ray <jason.ray@agilex.com>
2014-03-09 12:42:52 -0400

    formatting

--- src/test/java/rpn/MultiplyTest.java
+++ src/test/java/rpn/MultiplyTest.java
@@ -5,12 +5,12 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class MultiplyTest {
-	@Test
-	public void multiplyTwoNumbers() {
-		RpnCalculator calc = new RpnCalculator();
-		calc.enter(5);
-		calc.enter(4);
-		int result = calc.perform("*");
-		assertEquals(result, 20);
-	}
+    @Test
+    public void multiplyTwoNumbers() {
+        RpnCalculator calc = new RpnCalculator();
+        calc.enter(5);
+        calc.enter(4);
+        int result = calc.perform("*");
+        assertEquals(result, 20);
+    }
 }
