73408d0b8dc70b0ac7436488fa25886996401caf
Jason Ray <jason.ray@agilex.com>
2014-03-09 12:39:53 -0400

    formatting

--- src/main/java/rpn/operator/AbsoluteOperator.java
+++ src/main/java/rpn/operator/AbsoluteOperator.java
@@ -4,28 +4,28 @@ import rpn.RpnStack;
 
 public class AbsoluteOperator implements Operator {
 
-	@Override
-	public int doOperation(RpnStack numbers) {
-		int n = numbers.pop();
-		int result = getAbsolute(n);
-		numbers.push(result);
-		return result;
-	}
+    @Override
+    public int doOperation(RpnStack numbers) {
+        int n = numbers.pop();
+        int result = getAbsolute(n);
+        numbers.push(result);
+        return result;
+    }
 
-	private int getAbsolute(int n) {
-		int result;
-		if (n > 0)
-			result = n;
-		else if (n < 0)
-			result = -1 * n;
-		else
-			result = n;
-		return result;
-	}
+    private int getAbsolute(int n) {
+        int result;
+        if (n > 0)
+            result = n;
+        else if (n < 0)
+            result = -1 * n;
+        else
+            result = n;
+        return result;
+    }
 
-	@Override
-	public boolean handlesOperatorCharacter(String operand) {
-		return operand.contentEquals("||");
-	}
+    @Override
+    public boolean handlesOperatorCharacter(String operand) {
+        return operand.contentEquals("||");
+    }
 
 }
