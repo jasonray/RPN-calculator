c8c2e9d97f7f7ce66f8879fabda2ee8298052dd0
Jason Ray <jason.ray@agilex.com>
2014-03-09 12:39:43 -0400

    formatting

--- src/main/java/rpn/RpnStack.java
+++ src/main/java/rpn/RpnStack.java
@@ -3,33 +3,33 @@ package rpn;
 import java.util.Stack;
 
 public class RpnStack {
-	private Stack<Integer> stack = new Stack<Integer>();
+    private Stack<Integer> stack = new Stack<Integer>();
 
-	public void push(int n) {
-		stack.push(n);
-	}
+    public void push(int n) {
+        stack.push(n);
+    }
 
-	public int pop() {
-		int n;
-		if (isEmpty())
-			n = 0;
-		else
-			n = stack.pop();
-		return n;
-	}
+    public int pop() {
+        int n;
+        if (isEmpty())
+            n = 0;
+        else
+            n = stack.pop();
+        return n;
+    }
 
-	public int peek() {
-		int n;
-		if (isEmpty()) {
-			n = 0;
-		} else {
-			n = stack.peek();
-		}
-		return n;
-	}
+    public int peek() {
+        int n;
+        if (isEmpty()) {
+            n = 0;
+        } else {
+            n = stack.peek();
+        }
+        return n;
+    }
 
-	public boolean isEmpty() {
-		return stack.empty();
-	}
+    public boolean isEmpty() {
+        return stack.empty();
+    }
 
 }
