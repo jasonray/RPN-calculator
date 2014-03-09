436f64abaf27adc2656fd3044bd3441677c9d318
Jason Ray <jason.ray@agilex.com>
2014-03-09 12:42:43 -0400

    formatting

--- src/test/java/rpn/FactorialTest.java
+++ src/test/java/rpn/FactorialTest.java
@@ -6,27 +6,27 @@ import org.junit.Test;
 
 public class FactorialTest {
 
-	@Test
-	public void factorialReturnsProduct() {
-		RpnCalculator calc = new RpnCalculator();
-		calc.enter(4);
-		int result = calc.perform("!");
-		assertEquals(24, result);
-	}
+    @Test
+    public void factorialReturnsProduct() {
+        RpnCalculator calc = new RpnCalculator();
+        calc.enter(4);
+        int result = calc.perform("!");
+        assertEquals(24, result);
+    }
 
-	@Test
-	public void factorialOnNothing() {
-		RpnCalculator calc = new RpnCalculator();
-		int result = calc.perform("!");
-		assertEquals(1, result);
-	}
+    @Test
+    public void factorialOnNothing() {
+        RpnCalculator calc = new RpnCalculator();
+        int result = calc.perform("!");
+        assertEquals(1, result);
+    }
 
-	@Test
-	public void twoFactorials() {
-		RpnCalculator calc = new RpnCalculator();
-		calc.enter(3);
-		int result = calc.perform("!");
-		result = calc.perform("!");
-		assertEquals(720, result);
-	}
+    @Test
+    public void twoFactorials() {
+        RpnCalculator calc = new RpnCalculator();
+        calc.enter(3);
+        int result = calc.perform("!");
+        result = calc.perform("!");
+        assertEquals(720, result);
+    }
 }
