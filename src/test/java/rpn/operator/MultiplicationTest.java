7246a8e976911ed3500272f1560c37f59caa5383
jason.ray <jasonray@jayray.net>
2017-01-01 21:01:31 -0500

    autoformat

--- src/test/java/rpn/operator/MultiplicationTest.java
+++ src/test/java/rpn/operator/MultiplicationTest.java
@@ -9,44 +9,44 @@ public class MultiplicationTest {
     @Test
     public void multipleTwoNumbers() {
         RpnCalculator calc = new RpnCalculator();
-        calc.enter(2);
-        calc.enter(4);
-        int result = calc.perform("*");
-        assertEquals(8, result);
+        calc.enter( 2 );
+        calc.enter( 4 );
+        int result = calc.perform( "*" );
+        assertEquals( 8, result );
     }
 
     @Test
     public void multipleNegNumbers() {
         RpnCalculator calc = new RpnCalculator();
-        calc.enter(-2);
-        calc.enter(4);
-        int result = calc.perform("*");
-        assertEquals(-8, result);
+        calc.enter( -2 );
+        calc.enter( 4 );
+        int result = calc.perform( "*" );
+        assertEquals( -8, result );
     }
 
     @Test
     public void multiplyNoNumbers() {
         RpnCalculator calc = new RpnCalculator();
-        int result = calc.perform("*");
-        assertEquals(0, result);
+        int result = calc.perform( "*" );
+        assertEquals( 0, result );
     }
 
     @Test
     public void multiplyOneNumber() {
         RpnCalculator calc = new RpnCalculator();
-        calc.enter(1);
-        int result = calc.perform("*");
-        assertEquals(0, result);
+        calc.enter( 1 );
+        int result = calc.perform( "*" );
+        assertEquals( 0, result );
     }
 
     @Test
     public void multipleThreeNumbers() {
         RpnCalculator calc = new RpnCalculator();
-        calc.enter(2);
-        calc.enter(3);
-        calc.perform("*");
-        calc.enter(3);
-        int result = calc.perform("*");
-        assertEquals(18, result);
+        calc.enter( 2 );
+        calc.enter( 3 );
+        calc.perform( "*" );
+        calc.enter( 3 );
+        int result = calc.perform( "*" );
+        assertEquals( 18, result );
     }
 }
