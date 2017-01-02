ba5874d655a473c139e8331ecdf9534e09ecadef
jason.ray <jasonray@jayray.net>
2017-01-01 21:00:21 -0500

    fixed test formatting

--- src/test/java/rpn/operator/AbsoluteTest.java
+++ src/test/java/rpn/operator/AbsoluteTest.java
@@ -9,51 +9,51 @@ public class AbsoluteTest {
 
     @Test
     public void zero() {
-        runTest(0, 0);
+        runTest( 0, 0 );
     }
 
     @Test
     public void one() {
-        runTest(1, 1);
+        runTest( 1, 1 );
     }
 
     @Test
     public void negOne() {
-        runTest(1, -1);
+        runTest( 1, -1 );
     }
 
     @Test
     public void two() {
-        runTest(2, 2);
+        runTest( 2, 2 );
     }
 
     @Test
     public void negTwo() {
-        runTest(2, -2);
+        runTest( 2, -2 );
     }
 
     @Test
     public void absOnEmptyStack() {
         RpnCalculator calc = new RpnCalculator();
-        int result = calc.perform("||");
-        assertEquals(0, result);
+        int result = calc.perform( "||" );
+        assertEquals( 0, result );
     }
 
     private void runTest(int expected, int n) {
         RpnCalculator calc = new RpnCalculator();
-        calc.enter(n);
-        int result = calc.perform("||");
-        assertEquals(expected, result);
+        calc.enter( n );
+        int result = calc.perform( "||" );
+        assertEquals( expected, result );
     }
 
     @Test
     public void ensureResultIsOnStack() {
         RpnCalculator calc = new RpnCalculator();
-        calc.enter(1);
-        calc.enter(-5);
-        calc.perform("||");
+        calc.enter( 1 );
+        calc.enter( -5 );
+        calc.perform( "||" );
 
         // prove that 5 is on the stack by adding 1 to get 6
-        assertEquals(6, calc.perform("+"));
+        assertEquals( 6, calc.perform( "+" ) );
     }
 }
