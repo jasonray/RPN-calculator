56a686e177b74ddeab6cad9c89e75ca920550907
jason.ray <jasonray@jayray.net>
2017-01-01 21:01:09 -0500

    autoformat

--- src/test/java/rpn/operator/DivisionTest.java
+++ src/test/java/rpn/operator/DivisionTest.java
@@ -9,50 +9,50 @@ public class DivisionTest {
     @Test
     public void divideTwoNumbers() {
         RpnCalculator calc = new RpnCalculator();
-        calc.enter(8);
-        calc.enter(2);
-        int result = calc.perform("/");
-        assertEquals(4, result);
+        calc.enter( 8 );
+        calc.enter( 2 );
+        int result = calc.perform( "/" );
+        assertEquals( 4, result );
     }
 
     @Test
     public void divideOneNumber() {
         RpnCalculator calc = new RpnCalculator();
-        calc.enter(2);
-        int result = calc.perform("/");
-        assertEquals(0, result);
+        calc.enter( 2 );
+        int result = calc.perform( "/" );
+        assertEquals( 0, result );
     }
 
     @Test(expected = ArithmeticException.class)
     public void divideNoNumber() {
         RpnCalculator calc = new RpnCalculator();
-        calc.perform("/");
+        calc.perform( "/" );
     }
 
     @Test
     public void divideThreeNumber() {
         RpnCalculator calc = new RpnCalculator();
-        calc.enter(16);
-        calc.enter(8);
-        calc.enter(2);
-        int result = calc.perform("/");
-        assertEquals(4, result);
+        calc.enter( 16 );
+        calc.enter( 8 );
+        calc.enter( 2 );
+        int result = calc.perform( "/" );
+        assertEquals( 4, result );
     }
 
     @Test(expected = ArithmeticException.class)
     public void divideByZero() {
         RpnCalculator calc = new RpnCalculator();
-        calc.enter(6);
-        calc.enter(0);
-        calc.perform("/");
+        calc.enter( 6 );
+        calc.enter( 0 );
+        calc.perform( "/" );
     }
 
     @Test
     public void divideByFractionalNumber() {
         RpnCalculator calc = new RpnCalculator();
-        calc.enter(6);
-        calc.enter(4);
-        int result = calc.perform("/");
-        assertEquals(1, result);
+        calc.enter( 6 );
+        calc.enter( 4 );
+        int result = calc.perform( "/" );
+        assertEquals( 1, result );
     }
 }
