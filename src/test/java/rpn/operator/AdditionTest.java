92a040ce7fb3dda2e177f440fc7582eadbb13f56
jason.ray <jasonray@jayray.net>
2017-01-01 21:01:03 -0500

    autoformat

--- src/test/java/rpn/operator/AdditionTest.java
+++ src/test/java/rpn/operator/AdditionTest.java
@@ -9,44 +9,44 @@ public class AdditionTest {
     @Test
     public void addTwoNumbersReturnsSum() {
         RpnCalculator calc = new RpnCalculator();
-        calc.enter(30);
-        calc.enter(4);
-        int result = calc.perform("+");
-        assertEquals(34, result);
+        calc.enter( 30 );
+        calc.enter( 4 );
+        int result = calc.perform( "+" );
+        assertEquals( 34, result );
     }
 
     @Test
     public void addNoNumbers() {
         RpnCalculator calc = new RpnCalculator();
-        int result = calc.perform("+");
-        assertEquals(0, result);
+        int result = calc.perform( "+" );
+        assertEquals( 0, result );
     }
 
     @Test
     public void addOneNumber() {
         RpnCalculator calc = new RpnCalculator();
-        calc.enter(1);
-        int result = calc.perform("+");
-        assertEquals(1, result);
+        calc.enter( 1 );
+        int result = calc.perform( "+" );
+        assertEquals( 1, result );
     }
 
     @Test
     public void addThreeNumbersReturnsSum() {
         RpnCalculator calc = new RpnCalculator();
-        calc.enter(1);
-        calc.enter(2);
-        calc.perform("+");
-        calc.enter(3);
-        int result = calc.perform("+");
-        assertEquals(6, result);
+        calc.enter( 1 );
+        calc.enter( 2 );
+        calc.perform( "+" );
+        calc.enter( 3 );
+        int result = calc.perform( "+" );
+        assertEquals( 6, result );
     }
 
     @Test
     public void addNegNumbers() {
         RpnCalculator calc = new RpnCalculator();
-        calc.enter(1);
-        calc.enter(-2);
-        int result = calc.perform("+");
-        assertEquals(-1, result);
+        calc.enter( 1 );
+        calc.enter( -2 );
+        int result = calc.perform( "+" );
+        assertEquals( -1, result );
     }
 }
