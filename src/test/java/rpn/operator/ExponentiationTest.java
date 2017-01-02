ae32743303362f43c835f72cae4b620600931cf2
jason.ray <jasonray@jayray.net>
2017-01-01 21:01:24 -0500

    autoformat

--- src/test/java/rpn/operator/ExponentiationTest.java
+++ src/test/java/rpn/operator/ExponentiationTest.java
@@ -8,52 +8,52 @@ import static org.junit.Assert.assertEquals;
 public class ExponentiationTest {
     @Test
     public void raiseToPower() {
-        runTest(2, 3, 8);
+        runTest( 2, 3, 8 );
     }
 
     @Test
     public void raiseToZero() {
-        runTest(2, 0, 1);
+        runTest( 2, 0, 1 );
     }
 
     @Test
     public void oneRaised() {
-        runTest(1, 2, 1);
+        runTest( 1, 2, 1 );
     }
 
     @Test
     public void zeroRaisedToPositive() {
-        runTest(0, 2, 0);
+        runTest( 0, 2, 0 );
     }
 
     @Test(expected = ArithmeticException.class)
     public void zeroRaisedToNegative() {
-        calculate(0, -2);
+        calculate( 0, -2 );
     }
 
     @Test
     public void neg1ToOdd() {
-        runTest(-1, 3, -1);
+        runTest( -1, 3, -1 );
     }
 
     @Test
     public void neg1ToEven() {
-        runTest(-1, 4, 1);
+        runTest( -1, 4, 1 );
     }
 
     @Test
     public void raiseToNegPower() {
-        runTest(2, -2, (1 / 4));
+        runTest( 2, -2, (1 / 4) );
     }
 
     private void runTest(int lhs, int rhs, int expected) {
-        assertEquals(expected, calculate(lhs, rhs));
+        assertEquals( expected, calculate( lhs, rhs ) );
     }
 
     private int calculate(int lhs, int rhs) {
         RpnCalculator calc = new RpnCalculator();
-        calc.enter(lhs);
-        calc.enter(rhs);
-        return calc.perform("^");
+        calc.enter( lhs );
+        calc.enter( rhs );
+        return calc.perform( "^" );
     }
 }
