bba9bb7cc96ac3be5f62cea963ed7909471be2f7
jason.ray <jasonray@jayray.net>
2017-01-01 21:01:38 -0500

    autoformat

--- src/test/java/rpn/operator/SubtractTest.java
+++ src/test/java/rpn/operator/SubtractTest.java
@@ -9,45 +9,45 @@ public class SubtractTest {
     @Test
     public void subtractNoNumbers() {
         RpnCalculator calc = new RpnCalculator();
-        int result = calc.perform("-");
-        assertEquals(0, result);
+        int result = calc.perform( "-" );
+        assertEquals( 0, result );
     }
 
     @Test
     public void subtractOneNumbers() {
         RpnCalculator calc = new RpnCalculator();
-        calc.enter(1);
-        int result = calc.perform("-");
-        assertEquals(-1, result);
+        calc.enter( 1 );
+        int result = calc.perform( "-" );
+        assertEquals( -1, result );
     }
 
     @Test
     public void subtractOneNumberTwice() {
         RpnCalculator calc = new RpnCalculator();
-        calc.enter(1);
-        int result = calc.perform("-");
-        result = calc.perform("-");
-        assertEquals(1, result);
+        calc.enter( 1 );
+        int result = calc.perform( "-" );
+        result = calc.perform( "-" );
+        assertEquals( 1, result );
     }
 
     @Test
     public void subtractTwoNumbers() {
         RpnCalculator calc = new RpnCalculator();
-        calc.enter(20);
-        calc.enter(10);
-        int result = calc.perform("-");
-        assertEquals(10, result);
+        calc.enter( 20 );
+        calc.enter( 10 );
+        int result = calc.perform( "-" );
+        assertEquals( 10, result );
     }
 
     @Test
     public void subtractThreeNumbers() {
         RpnCalculator calc = new RpnCalculator();
-        calc.enter(21);
-        calc.enter(13);
-        calc.perform("-");
-        calc.enter(5);
+        calc.enter( 21 );
+        calc.enter( 13 );
+        calc.perform( "-" );
+        calc.enter( 5 );
 
-        int result = calc.perform("-");
-        assertEquals(3, result);
+        int result = calc.perform( "-" );
+        assertEquals( 3, result );
     }
 }
