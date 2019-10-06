c07c85d24d7fc395b1bd17c5d0cba50185abfb4a
Jason Ray <jasonray@jayray.net>
2019-10-05 20:53:56 -0400

    expand max test

--- src/test/java/rpn/operator/MaxTest.java
+++ src/test/java/rpn/operator/MaxTest.java
@@ -59,4 +59,16 @@ public class MaxTest {
         int result = calc.perform("max");
         assertEquals(5, result);
     }
+
+    @Test
+    public void maxSeveralNegativeNumbers() {
+        RpnCalculator calc = new RpnCalculator();
+        calc.enter(-2);
+        calc.enter(-1);
+        calc.enter(-3);
+        calc.enter(-4);
+        calc.enter(-5);
+        int result = calc.perform("max");
+        assertEquals(-1, result);
+    }
 }
