ccb115f7d01c0eddece855937defa27274a18040
Jason Ray <jasonray@jayray.net>
2019-10-05 20:23:05 -0400

    formatting

--- src/test/java/rpn/operator/DoubleTest.java
+++ src/test/java/rpn/operator/DoubleTest.java
@@ -9,37 +9,41 @@ public class DoubleTest {
     @Test
     public void doubleSingleNumber() {
         RpnCalculator calc = new RpnCalculator();
-        calc.enter( 4 );
-        int result = calc.perform( "double" );
-        assertEquals( 8, result );
+        calc.enter(4);
+        int result = calc.perform("double");
+        assertEquals(8, result);
     }
+
     @Test
     public void doubleNothing() {
         RpnCalculator calc = new RpnCalculator();
-        int result = calc.perform( "double" );
-        assertEquals( 0, result );
+        int result = calc.perform("double");
+        assertEquals(0, result);
     }
+
     @Test
     public void doulbePutsResultOnStack() {
         RpnCalculator calc = new RpnCalculator();
         calc.enter(4);
-        int result = calc.perform( "double" );
-        assertEquals( 8, calc.peek() );
+        int result = calc.perform("double");
+        assertEquals(8, calc.peek());
     }
+
     @Test
     public void executeDoubleTwice() {
         RpnCalculator calc = new RpnCalculator();
         calc.enter(4);
-        calc.perform( "double" );
-        calc.perform( "double" );
-        assertEquals( 16, calc.peek() );
+        calc.perform("double");
+        calc.perform("double");
+        assertEquals(16, calc.peek());
     }
+
     @Test
     public void doubleOnNegative() {
         RpnCalculator calc = new RpnCalculator();
         calc.enter(4);
-         calc.perform( "double" );
-        int result = calc.perform( "double" );
-        assertEquals( 16, result );
+        calc.perform("double");
+        int result = calc.perform("double");
+        assertEquals(16, result);
     }
 }
